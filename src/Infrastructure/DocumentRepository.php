de4b7291c7b027998e844c68d8b1b03cd46579fd
Danijel Maricic <danijel.maricic@gmail.com>
2015-08-18 14:32:05 +0300

    added most used methods to DocumentRepository

--- src/Infrastructure/DocumentRepository.php
+++ src/Infrastructure/DocumentRepository.php
@@ -2,8 +2,8 @@
 
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\DocumentRepository as MongoDocumentRepository;
-use Doctrine\ODM\MongoDB\UnitOfWork;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use Doctrine\ODM\MongoDB\UnitOfWork;
 
 class DocumentRepository extends MongoDocumentRepository
 {
@@ -13,9 +13,23 @@ class DocumentRepository extends MongoDocumentRepository
         parent::__construct($dm, $uow, $class);
     }
 
+
+    /**
+     * @param $name
+     *
+     * @return object
+     */
+    public function findByName($name)
+    {
+        return $this->findOneBy(['name' => $name]);
+    }
+
+
     /**
      * Get document by short ID
+     *
      * @param $id
+     *
      * @return object
      */
     public function findByShortId($id)
@@ -23,9 +37,12 @@ class DocumentRepository extends MongoDocumentRepository
         return $this->findOneBy(['shortId' => $id]);
     }
 
+
     /**
      * Get document(s) by a list of short IDs
-     * @param array  $ids List of short IDs
+     *
+     * @param array $ids List of short IDs
+     *
      * @return array
      */
     public function findByShortIdList($ids)
